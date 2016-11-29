6c483b6b7fcdb40841a25c97066d8c72db9a1b09
bakasajoshua <baksajoshua09@gmail.com>
2016-11-29 19:29:30 +0300

    Removal of the ineffective links

--- cp/add_edit_capacity.php
+++ cp/add_edit_capacity.php
@@ -8,7 +8,19 @@ if(isset($_POST['submitCapacity'])){
 	header("location:admin_capacity.php");	
 	exit;
 }
-include("header.php"); 
+include("../includes/db.conn.php");
+include("language.php");
+$path=pathinfo($_SERVER['PHP_SELF']);
+$filename=$path['basename'];
+$get_sub_title=mysql_query("select * from bsi_adminmenu where url='".$filename."'");
+if(mysql_num_rows($get_sub_title)){
+  $get_sub_title_row=mysql_fetch_array($get_sub_title);
+  $get_parent_title=mysql_query("select * from bsi_adminmenu where id='".$get_sub_title_row['parent_id']."'");
+  $get_parent_title_row=mysql_fetch_array($get_parent_title);
+  $main_title=$get_parent_title_row['name'].' > '.$get_sub_title_row['name'];
+  $_SESSION['main_title']=$main_title;
+}
+
 include("../includes/conf.class.php");
 include("../includes/admin.class.php");
 if(isset($_GET['id']) && $_GET['id'] != ""){
@@ -26,34 +38,123 @@ if(isset($_GET['id']) && $_GET['id'] != ""){
 	exit;
 }
 ?>  
- <link rel="stylesheet" type="text/css" href="css/jquery.validate.css" />
-      <div id="container-inside">
-      <span style="font-size:16px; font-weight:bold"><?php echo CAPACITY_ADD_AND_EDIT; ?></span>
-      <hr />
-        <form action="<?=$_SERVER['PHP_SELF']?>" method="post" id="form1">
-          <table cellpadding="5" cellspacing="2" border="0">
-            <tr>
-              <td><strong><?php echo CAPACITY_TITLE_ADD_EDIT; ?>:</strong></td>
-              <td valign="middle"><input type="text" name="capacity_title" id="capacity_title" class="required" value="<?=$row['title']?>" style="width:250px;" /> &nbsp;&nbsp;<?php echo EXAMPLE_SINGLE_DOUBLE;?></td>
-            </tr>
-            <tr>
-              <td><strong><?php echo ADD_EDIT_CAPACITY_NUMBER_OF_ADULT; ?>:</strong></td>
-              <td><input type="text" name="no_adult" id="no_adult" <?=$readonly?> value="<?=$row['capacity']?>" class="required number" style="width:70px;"  /></td>
-            </tr>
-            
-            
-              <td><input type="hidden" name="addedit" value="<?=$id?>"></td>
-              <td><input type="submit" value="<?php echo ADD_EDIT_CAPACITY_SUBMIT;?>" name="submitCapacity" style="background:#e5f9bb; cursor:pointer; cursor:hand;"/></td>
-            </tr>
-          </table>
-        </form>
-      </div>
-<script type="text/javascript">
-	$().ready(function() {
-		$("#form1").validate();
-		
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin Module</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../admin/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- DataTables CSS -->
+    <link href="../admin/vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="../admin/vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../admin/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../admin/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- jQuery -->
+    <script src="../admin/vendor/jquery/jquery.min.js"></script>
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+         <?php include_once('admin_nav.php');?>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header"><?php echo CAPACITY_ADD_AND_EDIT; ?></h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <?php echo CAPACITY_ADD_AND_EDIT; ?>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                          <form action="<?=$_SERVER['PHP_SELF']?>" method="post" id="form1">
+                                <div class="col-lg-6">       
+                                  <div class="form-group">
+                                      <label><?php echo CAPACITY_TITLE_ADD_EDIT; ?>:</label>
+                                      <input type="text" name="capacity_title" id="capacity_title" class="required digits form-control"  value="<?=$row['title']?>" size="15"/>&nbsp;&nbsp;<?php echo EXAMPLE_SINGLE_DOUBLE; ?>
+                                  </div>
+                                  <div class="form-group">
+                                      <label><?php echo ADD_EDIT_CAPACITY_NUMBER_OF_ADULT; ?>:</label>
+                                      <input type="text" name="no_adult" id="no_adult" <?=$readonly?> value="<?=$row['capacity']?>" class="required number form-control"  />
+                                  </div>
+                                  <input type="hidden" name="addedit" value="<?=$id?>">
+                                  <div class="form-group">
+                                     <input type="submit" value="<?php echo ADD_EDIT_CAPACITY_SUBMIT;?>" name="sbt_details" id="sbt_details" class="btn btn-primary"/>
+                                  </div>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../admin/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../admin/vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- DataTables JavaScript -->
+    <script src="../admin/vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="../admin/vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+    <script src="../admin/vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../admin/dist/js/sb-admin-2.js"></script>
+
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $("#form1").validate();
+        $("#roomtype_id").attr("class","form-control");
+        $("#capacity_id").attr("class","form-control");
      });
          
-</script>      
+</script> 
 <script src="js/jquery.validate.js" type="text/javascript"></script>
-<?php include("footer.php"); ?> 
+<?php include("footer.php"); ?>
